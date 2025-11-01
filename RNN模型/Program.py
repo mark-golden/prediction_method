c41141e81ebf026d0f2d77ac50aaaa312480a686
GRZ <grzreceive@163.com>
2025-11-01 11:38:19 +0800

    增加代码

---
+++ RNN模型/Program.py
@@ -0,0 +1,37 @@
+
+"""
+程序作者: GRZ
+程序时间: 2025-11-01
+程序目的: PyTorch实现RNN模型的基本框架
+"""
+
+# Import Library
+import os
+import logging
+import logging.config
+## logging设置
+### 载入配置文件
+logging.config.fileConfig('config.ini')
+### 获取logger
+#### 输出到控制台
+consolelogger = logging.getLogger('ConsoleOutput')
+#### 输出到文件
+filelogger = logging.getLogger()
+
+"""
+	函数功能: 主程序
+	输入: 
+	输出:  
+"""
+def main():
+	pass
+
+"""
+	函数功能: 主程序入口
+	输入: 
+	输出: 
+"""
+if __name__ == '__main__':
+	main()
+
+
